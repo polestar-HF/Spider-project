7e33ca0810352126fe820e078a34b6261652d419
polestar-HF <1575174074@qq.com>
2021-10-16 22:26:54 +0800

    ✨add yuanrenxue top5

---
+++ 5-乱码增强/hook.js
@@ -0,0 +1,8 @@
+(function (){
+    var ko = _0x37614a
+    _0x37614a = function (_0x32e7c1){
+        var result = ko(_0x32e7c1)
+        console.log('加密时间戳：',_0x32e7c1, '加密结果：', result)
+        return result
+    }
+})()
